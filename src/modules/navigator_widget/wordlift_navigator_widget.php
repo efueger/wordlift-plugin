f30c53d9744b92c6aee6687b92ff4d7b5f159562
pieroit <savastanos@libero.it>
2015-01-03 12:56:51 +0100

    get post title and thumb in the navigator

--- src/modules/navigator_widget/wordlift_navigator_widget.php
+++ src/modules/navigator_widget/wordlift_navigator_widget.php
@@ -17,25 +17,38 @@ function wordlift_shortcode_navigator() {
             'slick-css', plugins_url( 'js-client/slick/slick.css', __FILE__ )
     );
 
-    // get the current post id.
-    $post_id = get_the_ID();
+    
+    
+    // get the current post.
+    $post = get_post( get_the_ID() );
     $depth = 5;
     
     // get the related posts and entities.
-    $related_posts = wl_shortcode_chord_get_relations( $post_id, $depth );
+    $related_posts = wl_shortcode_chord_get_relations( $post->ID, $depth );
     var_dump($related_posts);
     
-    $content = '<div class="your-class">';
-    foreach ( $related_posts as $related_post ) {
-        $content .= '<div><img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSEHnG0mWIkoluzMx0KeHxFjdNYTr2vPwulkxRh11j6aFYytld9iw"></div>';
+    $content = '<div id="wl-navigator-widget">';
+    foreach ( $related_posts['entities'] as $related_post_id ) {
+        var_dump($related_post);
+        $related_post = get_post( $related_post_id );
+        $thumb = get_the_post_thumbnail( $related_post_id, 'thumbnail' );
+        if( empty( $thumb ) ) {
+            $thumb = '<img />';
+        }
+        $content .= '<div class="wl-navigator-card">'
+            . $thumb .
+            '<span class="wl-navigator-trigger">' . $related_post->post_title . '</span>
+            <span class="wl-navigator-contest">' . $related_post->post_title . '</span>
+        </div>';
     }
     $content .= '</div>';
 
     $content .= '<script>$=jQuery; $(document).ready(function(){
-            $(".your-class").slick({
+            $(".wl-navigator-card > img").addClass("wl-navigator-lens");
+            $("#wl-navigator-widget").slick({
                 dots: true,
                 infinite: true,
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1
             });
         });
