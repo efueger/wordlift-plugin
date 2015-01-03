47025c34b79c05012f2ad6ab4b372e0302c71278
pieroit <savastanos@libero.it>
2015-01-03 16:12:21 +0100

    missing image file for the navigator

--- src/modules/navigator_widget/wordlift_navigator_widget.php
+++ src/modules/navigator_widget/wordlift_navigator_widget.php
@@ -16,35 +16,39 @@ function wordlift_shortcode_navigator() {
     wp_enqueue_style(
             'slick-css', plugins_url( 'js-client/slick/slick.css', __FILE__ )
     );
-
-    
-    
+        
     // get the current post.
     $post = get_post( get_the_ID() );
     $depth = 5;
     
     // get the related posts and entities.
-    $related_posts = wl_shortcode_chord_get_relations( $post->ID, $depth );
-    var_dump($related_posts);
+    $related_posts = wl_get_referenced_entity_ids( $post->ID );
+    var_dump( $related_posts );
     
     $content = '<div id="wl-navigator-widget">';
-    foreach ( $related_posts['entities'] as $related_post_id ) {
-        var_dump($related_post);
+    foreach ( $related_posts as $related_post_id ) {
         $related_post = get_post( $related_post_id );
         $thumb = get_the_post_thumbnail( $related_post_id, 'thumbnail' );
         if( empty( $thumb ) ) {
-            $thumb = '<img />';
+            $thumb = '<img src="' . plugins_url('js-client/slick/missing-image-150x150.png', __FILE__ ) . '" />';
         }
         $content .= '<div class="wl-navigator-card">'
             . $thumb .
-            '<span class="wl-navigator-trigger">' . $related_post->post_title . '</span>
-            <span class="wl-navigator-contest">' . $related_post->post_title . '</span>
+            '<a class="wl-navigator-trigger" href="' . get_permalink( $related_post_id ) . '">' . $related_post->post_title . '</a>
+            <a class="wl-navigator-contest">' . $related_post->post_title . '</a>
         </div>';
     }
     $content .= '</div>';
 
     $content .= '<script>$=jQuery; $(document).ready(function(){
+
+            // Some css fixing
             $(".wl-navigator-card > img").addClass("wl-navigator-lens");
+            setInterval( function(){
+                $(".slick-prev, .slick-next").css("background", "gray");
+            }, 500);
+            
+            // Launch navigator
             $("#wl-navigator-widget").slick({
                 dots: true,
                 infinite: true,
